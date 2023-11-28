6f556afda2df6649dc4f3d8f4e467bb1f41523fb
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-11-28 22:15:01 +0600

    add all initial files

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const logger = require("./src/middleware/logger");
+require("dotenv").config();
+
+const port = process.env.PORT || 5001;
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "PATCH", "OPTIONS"],
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", logger, (req, res) => {
+  res.send({
+    message: "Server is running",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port:${port}`);
+});

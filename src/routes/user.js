1cf32532d722fa0c86047a3c41f0df6b3fa33132
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-12-01 20:23:28 +0600

    add user profile route

--- src/routes/user.js
+++ src/routes/user.js
@@ -12,4 +12,14 @@ route.post("/", async (req, res) => {
   }
 });
 
+route.get("/:userID", async (req, res) => {
+  try {
+    const userID = req.params.userID;
+    const result = await userColl.findOne({ userToken: userID });
+    res.status(200).send(result);
+  } catch (err) {
+    res.status(500).send("an error occurred");
+  }
+});
+
 module.exports = route;

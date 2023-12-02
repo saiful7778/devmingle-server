c7ff71d9231a4a3d59520f4779c2cab601a7ec88
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-12-02 20:46:19 +0600

    add post delete route

--- src/routes/post.js
+++ src/routes/post.js
@@ -2,15 +2,16 @@ const express = require("express");
 const { postColl, userColl } = require("../DB/mongodb");
 const verifyToken = require("../middleware/verifyToken");
 const verifyTokenAndKey = require("../middleware/verifyTokenKey");
+const { ObjectId } = require("mongodb");
 const route = express.Router();
 
-route.post("/:userID", verifyToken, verifyTokenAndKey, async (req, res) => {
+route.post("/", verifyToken, verifyTokenAndKey, async (req, res) => {
   try {
-    const userID = req.params.userID;
+    const { uid } = req.query;
     const postData = req.body;
 
     const filter = {
-      userToken: userID,
+      userToken: uid,
       $or: [{ badge: "bronze", postCount: { $lt: 5 } }, { badge: "gold" }],
     };
     const updatePostCount = await userColl.updateOne(filter, {
@@ -22,6 +23,22 @@ route.post("/:userID", verifyToken, verifyTokenAndKey, async (req, res) => {
 
     const result = await postColl.insertOne(postData);
     res.status(201).send({ message: true, result });
+  } catch (err) {
+    res.status(500).send("an error occurred");
+  }
+});
+
+route.delete("/:postID", verifyToken, verifyTokenAndKey, async (req, res) => {
+  try {
+    const { email, uid } = req.query;
+    const postID = req.params.postID;
+    const filter = { _id: new ObjectId(postID), "author.email": email };
+    const decrementPostCount = await userColl.updateOne(
+      { userEmail: email, userToken: uid },
+      { $inc: { postCount: -1 } }
+    );
+    const result = await postColl.deleteOne(filter);
+    res.send({ result, decrementPostCount });
   } catch (err) {
     res.status(500).send("an error occurred");
   }

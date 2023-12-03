d7f765790bd5f16d9ba281563568ffcb648e54cd
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-12-03 23:19:08 +0600

    add all user data route

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userColl } = require("../DB/mongodb");
 const verifyToken = require("../middleware/verifyToken");
 const verifyTokenAndKey = require("../middleware/verifyTokenKey");
+const { ObjectId } = require("mongodb");
 const route = express.Router();
 
 route.post("/", async (req, res) => {
@@ -22,10 +23,31 @@ route.post("/", async (req, res) => {
   }
 });
 
+route.get("/all", verifyToken, verifyTokenAndKey, async (req, res) => {
+  try {
+    const result = await userColl.find().toArray();
+    res.status(200).send(result);
+  } catch (err) {
+    res.status(500).send("an error occurred");
+  }
+});
+
 route.get("/:userID", verifyToken, verifyTokenAndKey, async (req, res) => {
   try {
     const userID = req.params.userID;
     const result = await userColl.findOne({ userToken: userID });
+    res.status(200).send(result);
+  } catch (err) {
+    res.status(500).send("an error occurred");
+  }
+});
+
+route.delete("/:userID", verifyToken, verifyTokenAndKey, async (req, res) => {
+  try {
+    const userID = req.params.userID;
+    const { uid } = req.query;
+    const query = { _id: new ObjectId(userID), userToken: uid };
+    const result = await userColl.findOne(query);
     res.status(200).send(result);
   } catch (err) {
     res.status(500).send("an error occurred");

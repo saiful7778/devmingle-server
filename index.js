b66ddc6d7b4a7426c7f7dc09f702e6f92c3cbd88
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-12-03 12:54:56 +0600

    add google recaptcha

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 const jwtRoute = require("./src/routes/jwtAuth");
 const userRoute = require("./src/routes/user");
 const postRoute = require("./src/routes/post");
+const recaptchaRoute = require("./src/routes/reCaptcha");
 
 const port = process.env.PORT || 5001;
 
@@ -31,6 +32,7 @@ app.get("/", logger, (req, res) => {
 app.use("/jwt", jwtRoute);
 app.use("/user", userRoute);
 app.use("/post", postRoute);
+app.use("/captcha", recaptchaRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);

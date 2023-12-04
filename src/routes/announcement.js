6dcf602fac0a8941328fd4ffae71274a97c87ab9
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-12-04 17:59:50 +0600

    announcement route to create read and delete

--- src/routes/announcement.js
+++ src/routes/announcement.js
@@ -3,6 +3,7 @@ const verifyToken = require("../middleware/verifyToken");
 const verifyTokenAndKey = require("../middleware/verifyTokenKey");
 const verifyAdmin = require("../middleware/verifyAdmin");
 const { announcementColl } = require("../DB/mongodb");
+const { ObjectId } = require("mongodb");
 const route = express.Router();
 
 route.post(
@@ -15,6 +16,41 @@ route.post(
       const data = req.body;
       const result = await announcementColl.insertOne(data);
       res.status(201).send(result);
+    } catch (err) {
+      res.status(500).send("an error occurred");
+    }
+  }
+);
+
+route.get("/count", async (req, res) => {
+  try {
+    const count = await announcementColl.estimatedDocumentCount();
+    res.status(200).send({ count });
+  } catch (err) {
+    res.status(500).send("an error occurred");
+  }
+});
+
+route.get("/", async (req, res) => {
+  try {
+    const result = await announcementColl.find().toArray();
+    res.status(200).send(result);
+  } catch (err) {
+    res.status(500).send("an error occurred");
+  }
+});
+
+route.delete(
+  "/:announcementID",
+  verifyToken,
+  verifyTokenAndKey,
+  verifyAdmin,
+  async (req, res) => {
+    try {
+      const announcementID = req.params.announcementID;
+      const query = { _id: new ObjectId(announcementID) };
+      const result = await announcementColl.deleteOne(query);
+      res.status(200).send(result);
     } catch (err) {
       res.status(500).send("an error occurred");
     }
